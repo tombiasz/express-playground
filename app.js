3059cfe166e75f69fe71a0d173a0994f7da50cb9
tombiasz <tomasz.m.nowacki@gmail.com>
2018-04-01 18:26:56 +0200

    configure database connection

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+// configure dotenv
+dotenv.config();
+
 var app = express();
+
+// database config
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGODB_URL;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
