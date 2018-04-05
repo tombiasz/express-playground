77f57eb7e5a9a9c5c249df7e0bff5580fe2b6827
tombiasz <tomasz.m.nowacki@gmail.com>
2018-04-05 18:05:29 +0200

    add book instance delete page

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -78,14 +78,21 @@ exports.bookinstance_create_post = [
   }
 ];
 
-// Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = function (req, res, next) {
+  BookInstance.findById(req.params.id)
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+
+      res.render('bookinstance_delete', { title: 'Delete BookInstance', bookinstance: bookinstance} );
+  });
 };
 
-// Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance delete POST');
+  BookInstance.findByIdAndRemove(req.body.bookinstanceid, function deleteBookInstance(err) {
+    if (err) { return next(err); }
+
+    res.redirect('/catalog/bookinstances');
+  });
 };
 
 // Display BookInstance update form on GET.
