8c7224c4d1d902bed26389a7083c7109edd90762
tombiasz <tomasz.m.nowacki@gmail.com>
2018-04-07 14:04:07 +0200

    refactor: use constants for statuses

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -3,11 +3,21 @@ const mongoose = require('mongoose');
 
 
 const { Schema } = mongoose;
+const STATUS_AVAILABLE = 'Available';
+const STATUS_MAINTENANCE = 'Maintenance';
+const STATUS_LOANED = 'Loaned';
+const STATUS_RESERVED = 'Reserved';
+const STATUSES = [
+  STATUS_AVAILABLE,
+  STATUS_MAINTENANCE,
+  STATUS_LOANED,
+  STATUS_RESERVED,
+];
 
 const BookInstanceSchema = new Schema({
   book: { type: Schema.ObjectId, ref: 'Book', required: true },
   imprint: { type: String, required: true },
-  status: { type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance' },
+  status: { type: String, required: true, enum: STATUSES, default: STATUS_MAINTENANCE },
   due_back: { type: Date, default: Date.now },
 });
 
